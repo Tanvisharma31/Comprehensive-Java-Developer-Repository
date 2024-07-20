1860ae0003e529add2f2b78d2911f7028242458e
tanvisharma31 <tanvi.sharma2021@sait.ac.in>
2024-07-20 22:01:04 +0530

    new file:   Beginners/Basic.java 	new file:   Beginners/DataTypes.java

---
+++ Beginners/DataTypes.java
@@ -0,0 +1,20 @@
+package Beginners;
+public class DataTypes{
+    public static void main(String[] args) {
+        int x = 10; // Integer variable
+        double y = 5.5; // Double variable
+        char z = 'a'; // Character variable
+        long a = 10000000000L; // Long variable
+        boolean b = true; // Boolean variable
+        String str = "Hello, World!"; // String variable
+        float f = 1.1f; // Float variable
+        // Print the values of the variables
+        System.out.println("Integer variable: " + x);
+        System.out.println("Double variable: " + y);
+        System.out.println("Character variable: " + z);
+        System.out.println("Long variable: " + a);
+        System.out.println("Boolean variable: " + b);
+        System.out.println("String variable: " + str);
+        System.out.println("Float variable: " + f);
+    }
+}
